94d0b150f6aaf87ee19cfb9989b2ec77ab4615bf
HanJin Park <phj9020@hotmail.com>
2021-03-24 16:49:44 +0900

    use axios & detail & suggestion URL, function added on resolver and schema

--- graphql/db.js
+++ graphql/db.js
@@ -1,10 +1,54 @@
+import axios from 'axios';
 import fetch from 'node-fetch';
 
-// https://yts.mx/api/v2/list_movies.json?limit=50&minimum_rating=9
 
-const API_URL = "https://yts.mx/api/v2/list_movies.json?"
+// Axios 
 
-// Wrap REST API with GraphQL
+const LIST_URL = "https://yts.mx/api/v2/list_movies.json"
+const DETAIL_URL = "https://yts.mx/api/v2/movie_details.json"
+const SUGGESTION_URL = "https://yts.mx/api/v2/movie_suggestions.json"
+
+
+
+export const getMovies = async (limit, rating) => {
+    const {data : { data : { movies }}} = await axios.get(LIST_URL, {
+        params: { 
+            limit: limit,
+            minimum_rating: rating
+        }
+    });
+    return movies;
+}
+
+export const getMovie = async (id) => {
+    const { data: { data: { movie } } } = await axios.get(DETAIL_URL, {
+        params: {
+            movie_id: id
+        }
+    });
+    return movie;
+}
+
+export const getSuggestion = async (id) => {
+    const {data : { data : { movies }}} = await axios.get(SUGGESTION_URL, {
+        params: {
+            movie_id: id
+        }
+    });
+    return movies;
+}
+
+
+
+// Wrap REST API with GraphQL 
+// USING FETCH 
+
+// const API_URL = "https://yts.mx/api/v2/list_movies.json?"
+// const DETAIL_URL = "https://yts.mx/api/v2/movie_details.json?"
+// const SUGGESTION_URL = "https://yts.mx/api/v2/movie_suggestions.json?"
+
+/*
+
 export const getMovies = (limit, rating) => {
     let REQUEST_URL = API_URL; 
     if(limit > 0) {
@@ -18,3 +62,25 @@ export const getMovies = (limit, rating) => {
     .then(json => json.data.movies)
 }
 
+export const getMovie = (id) => {
+    let REQUEST_URL = DETAIL_URL;
+    if(id) {
+        REQUEST_URL += `movie_id=${id}`
+    }
+    return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movie)
+}
+
+export const getSuggestion = (id) => {
+    let REQUEST_URL = SUGGESTION_URL;
+    if(id) {
+        REQUEST_URL += `movie_id=${id}`
+    }
+
+    return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies)
+}
+
+*/
--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,8 +1,10 @@
-import {getMovies} from './db';
+import {getMovies, getMovie, getSuggestion} from './db';
 
 const resolver = {
     Query: {
-        movies: (_, {limit, rating}) => getMovies(limit, rating)
+        movies: (_, {limit, rating}) => getMovies(limit, rating),
+        movie: (_, {id}) => getMovie(id),
+        suggestions: (_, {id}) => getSuggestion(id)
     }
 }
 
